64f03f80dfcd8322f46155d6d4ebb253491e99a2
Danny Hermes <daniel.j.hermes@gmail.com>
2016-02-16 12:49:22 -0800

    Using sympy.interpolating_poly() more effectively.

--- class_preso/weno_computations.py
+++ class_preso/weno_computations.py
@@ -7,35 +7,6 @@ import six
 import sympy
 
 
-X_SYMB = sympy.Symbol('X')
-
-
-def poly_interp(values, start_index):
-    r"""Interpolate a polynomial at ``values``.
-
-    Assumes :math:`\Delta x = 1` and uses ``start_index``
-    as the beginning value of :math:`j`.
-
-    :type values: list
-    :param values: List of values being interpolated.
-
-    :type start_index: int
-    :param start_index: The index where the stencil starts.
-
-    :rtype: :class:`sympy.core.expr.Expr`
-    :returns: The interpolated function.
-    """
-    num_values = len(values)
-    interp_poly = sympy.interpolating_poly(num_values, X_SYMB)
-    var_subs = {}
-    for index in six.moves.xrange(num_values):
-        x_var = sympy.Symbol('x%d' % (index,))
-        var_subs[x_var] = index + start_index
-        y_var = sympy.Symbol('y%d' % (index,))
-        var_subs[y_var] = values[index]
-    return interp_poly.subs(var_subs)
-
-
 def to_latex(value, replace_dict):
     """Convert an expression to LaTeX.
 
@@ -83,18 +54,24 @@ def interp_three_points():
         'E': 'u_{j+2}',
     }
     # Approximate with [-2, -1, 0].
-    p2_minus2 = poly_interp([u_minus2, u_minus1, u_zero], -2)
+    approx_minus2 = sympy.interpolating_poly(
+        3, one_half, X=[-2, -1, 0],
+        Y=[u_minus2, u_minus1, u_zero])
     approx_minus2 = sympy.Equality(sympy.Symbol(r'u_{j + \frac{1}{2}}^{(1)}'),
-                                   p2_minus2.subs({X_SYMB: one_half}))
+                                   approx_minus2)
     approx_minus2 = to_latex(approx_minus2, replace_dict)
     # Approximate with [-1, 0, 1].
-    p2_minus1 = poly_interp([u_minus1, u_zero, u_plus1], -1)
+    approx_minus1 = sympy.interpolating_poly(
+        3, one_half, X=[-1, 0, 1],
+        Y=[u_minus1, u_zero, u_plus1])
     approx_minus1 = sympy.Equality(sympy.Symbol(r'u_{j + \frac{1}{2}}^{(2)}'),
-                                   p2_minus1.subs({X_SYMB: one_half}))
+                                   approx_minus1)
     approx_minus1 = to_latex(approx_minus1, replace_dict)
     # Approximate with [0, 1, 2].
-    p2_zero = poly_interp([u_zero, u_plus1, u_plus2], 0)
+    approx_zero = sympy.interpolating_poly(
+        3, one_half, X=[0, 1, 2],
+        Y=[u_zero, u_plus1, u_plus2])
     approx_zero = sympy.Equality(sympy.Symbol(r'u_{j + \frac{1}{2}}^{(3)}'),
-                                 p2_zero.subs({X_SYMB: one_half}))
+                                 approx_zero)
     approx_zero = to_latex(approx_zero, replace_dict)
     return approx_minus2, approx_minus1, approx_zero
