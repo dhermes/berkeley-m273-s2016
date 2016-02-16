fb92545c71ca7d7de0a0951705a8096f91c999a3
Danny Hermes <daniel.j.hermes@gmail.com>
2016-02-16 10:54:32 -0800

    Updating class preso. with LaTeX hack to get variable ordering.

--- class_preso/weno_computations.py
+++ class_preso/weno_computations.py
@@ -37,6 +37,30 @@ def poly_interp(values, start_index):
     return interp_poly.subs(var_subs)
 
 
+def to_latex(value, replace_dict):
+    """Convert an expression to LaTeX.
+
+    This method is required so we can get a specified ordering
+    for terms that may not have the desired lexicographic
+    ordering.
+
+    :type value: :class:`sympy.core.expr.Expr`
+    :param value: A
+
+    :type replace_dict: dict
+    :param replace_dict: Dictionary where keys are old variable names (as
+                         strings) and values are the new variable names
+                         to replace them with.
+
+    :rtype: str
+    :returns: The value as LaTeX, with all variables replaced.
+    """
+    result = sympy.latex(value)
+    for old, new_ in six.iteritems(replace_dict):
+        result = result.replace(old, new_)
+    return result
+
+
 def interp_three_points():
     r"""Return interpolated values for :math:`x_{j+1/2}` using three points.
 
@@ -44,22 +68,33 @@ def interp_three_points():
     :math:`x_{j-1}, x_j, x_{j+1}` and :math:`x_{j}, x_{j+1}, x_{j+2}`.
 
     :rtype: tuple
-    :returns: Triple of :mod:`sympy` expressions, one for each set of
+    :returns: Triple of LaTeX strings, one for each set of
               interpolating points.
     """
     one_half = sympy.Rational(1, 2)
-    u_minus2, u_minus1, u0, u1, u2 = sympy.symbols(
-        'u_{j-2}, u_{j-1}, u_{j}, u_{j+1}, u_{j+2}')
+    # Intentionally use values which are simple to replace and
+    # ordered lexicographically.
+    u_minus2, u_minus1, u0, u1, u2 = sympy.symbols('A, B, C, D, E')
+    replace_dict = {
+        'A': 'u_{j-2}',
+        'B': 'u_{j-1}',
+        'C': 'u_{j}',
+        'D': 'u_{j+1}',
+        'E': 'u_{j+2}',
+    }
     # Approximate with [-2, -1, 0].
     p2_minus2 = poly_interp([u_minus2, u_minus1, u0], -2)
     approx_minus2 = sympy.Equality(sympy.Symbol(r'u_{j + \frac{1}{2}}^{(1)}'),
                                    p2_minus2.subs({X_SYMB: one_half}))
+    approx_minus2 = to_latex(approx_minus2, replace_dict)
     # Approximate with [-1, 0, 1].
     p2_minus1 = poly_interp([u_minus1, u0, u1], -1)
     approx_minus1 = sympy.Equality(sympy.Symbol(r'u_{j + \frac{1}{2}}^{(2)}'),
                                    p2_minus1.subs({X_SYMB: one_half}))
+    approx_minus1 = to_latex(approx_minus1, replace_dict)
     # Approximate with [0, 1, 2].
     p2_0 = poly_interp([u0, u1, u2], 0)
     approx_0 = sympy.Equality(sympy.Symbol(r'u_{j + \frac{1}{2}}^{(3)}'),
                               p2_0.subs({X_SYMB: one_half}))
+    approx_0 = to_latex(approx_0, replace_dict)
     return approx_minus2, approx_minus1, approx_0
