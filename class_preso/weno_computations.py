7f8fbcedaadfd560fbb07b99f1b71eccfd324ac2
Danny Hermes <daniel.j.hermes@gmail.com>
2016-02-17 20:14:54 -0800

    Adding reveal for the interpolation procedure.

--- class_preso/weno_computations.py
+++ class_preso/weno_computations.py
@@ -3,6 +3,9 @@
 
 from __future__ import print_function
 
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn
 import six
 import sympy
 
@@ -37,10 +40,10 @@ def interp_simple_stencils():
     First uses three sets of interpolating points,
     :math:`\left\{x_{j-2}, x_{j-1}, x_j\right\}`,
     :math:`\left\{x_{j-1}, x_j, x_{j+1}\right\}` and
-    :math:`\left\{x_{j}, x_{j+1}, x_{j+2}\right}`
+    :math:`\left\{x_{j}, x_{j+1}, x_{j+2}\right\}`
     to give local order three approximations.
 
-    Then uses all five points to
+    Then uses all five points
     :math:`\left\{x_{j-2}, x_{j-1}, x_j, x_{j+1}, x_{j+2}\right\}` to give
     an order five approximation on the whole stencil.
 
@@ -89,3 +92,163 @@ def interp_simple_stencils():
                                 approx_all)
     approx_all = to_latex(approx_all, replace_dict)
     return approx_minus2, approx_minus1, approx_zero, approx_all
+
+
+def make_intro_plots(stopping_point=None):
+    """Make introductory plots.
+
+    0, 3, 2, -1, 2
+    """
+    colors = seaborn.color_palette('husl')[:4]
+    fontsize = 16
+    num_pts = 100
+    rows = cols = 2
+    fig, ax_vals = plt.subplots(rows, cols, sharex=True, sharey=True)
+    fig.set_size_inches(15, 8)
+    ax_vals[0, 0].set_xlim(-3, 3)
+    ax_vals[0, 0].set_ylim(-3, 4)
+
+    # Top left plot (-2, -1, and 0)
+    top_left = ax_vals[0, 0]
+    top_left.plot(np.linspace(-2.5, -1.5, num_pts),
+                  0 * np.ones(num_pts),
+                  color='black', linestyle='dashed')
+    top_left.plot(np.linspace(-1.5, -0.5, num_pts),
+                  3 * np.ones(num_pts),
+                  color='black', linestyle='dashed')
+    top_left.plot(np.linspace(-0.5, 0.5, num_pts),
+                  2 * np.ones(num_pts),
+                  color='black', linestyle='dashed')
+    if stopping_point == 0:
+        return
+    label1 = (r'$u_{-2} \left(\frac{x^2 + x}{2}\right) +'
+              r'u_{-1} \left(-x^2 - 2x\right) + '
+              r'u_{0} \left(\frac{x^2 + 3x + 2}{2}\right)$')
+    top_left.text(-2.5, -2, label1, fontsize=fontsize)
+    x_vals = np.linspace(-2, 0, num_pts)
+    y_vals = -2 * x_vals**2 - 3 * x_vals + 2
+    top_left.plot(x_vals, y_vals, color=colors[0])
+    if stopping_point == 1:
+        return
+    # Do a little bit extra (a dashed line to 0.5 and then
+    # a dot at 0.5).
+    x_vals = np.linspace(0, 0.5, num_pts)
+    y_vals = -2 * x_vals**2 - 3 * x_vals + 2
+    top_left.plot(x_vals, y_vals, color=colors[0],
+                  linestyle='dashed')
+    y_val1 = y_vals[-1]
+    top_left.plot([0.5], [y_val1], color=colors[0],
+                  linestyle='None', marker='o')
+    label1_half = (r'$\frac{3}{8}u_{-2} - \frac{5}{4}u_{-1} + '
+                   r'\frac{15}{8}u_{0}$')
+    top_left.text(0.75, 1.9, label1_half, fontsize=fontsize)
+    if stopping_point == 2:
+        return
+
+    # Top right plot (-1, 0, and 1)
+    top_right = ax_vals[0, 1]
+    top_right.plot(np.linspace(-1.5, -0.5, num_pts),
+                   3 * np.ones(num_pts),
+                   color='black', linestyle='dashed')
+    top_right.plot(np.linspace(-0.5, 0.5, num_pts),
+                   2 * np.ones(num_pts),
+                   color='black', linestyle='dashed')
+    top_right.plot(np.linspace(0.5, 1.5, num_pts),
+                   -1 * np.ones(num_pts),
+                   color='black', linestyle='dashed')
+    if stopping_point == 3:
+        return
+    label2 = (r'$u_{-1} \left(\frac{x^2 - x}{2}\right) +'
+              r'u_{0} \left(1 -x^2\right) + '
+              r'u_{1} \left(\frac{x^2 + x}{2}\right)$')
+    top_right.text(-2.5, -2, label2, fontsize=fontsize)
+    x_vals = np.linspace(-1, 1, num_pts)
+    y_vals = -x_vals**2 - 2 * x_vals + 2
+    top_right.plot(x_vals, y_vals, color=colors[1])
+    if stopping_point == 4:
+        return
+    # Do a little bit extra: a dot at 0.5.
+    y_val2 = -0.5**2 - 2 * 0.5 + 2
+    top_right.plot([0.5], [y_val2], color=colors[1],
+                   linestyle='None', marker='o')
+    label2_half = (r'$-\frac{1}{8}u_{-1} + \frac{3}{4}u_{0} + '
+                   r'\frac{3}{8}u_{1}$')
+    top_right.text(0.75, 1.9, label2_half, fontsize=fontsize)
+    if stopping_point == 5:
+        return
+
+    # Bottom left plot (0, 1, 2)
+    bottom_left = ax_vals[1, 0]
+    bottom_left.plot(np.linspace(-0.5, 0.5, num_pts),
+                     2 * np.ones(num_pts),
+                     color='black', linestyle='dashed')
+    bottom_left.plot(np.linspace(0.5, 1.5, num_pts),
+                     -1 * np.ones(num_pts),
+                     color='black', linestyle='dashed')
+    bottom_left.plot(np.linspace(1.5, 2.5, num_pts),
+                     2 * np.ones(num_pts),
+                     color='black', linestyle='dashed')
+    if stopping_point == 6:
+        return
+    label3 = (r'$u_{0} \left(\frac{x^2 - 3x + 2}{2}\right) +'
+              r'u_{1} \left(-x^2 + 2x\right) + '
+              r'u_{2} \left(\frac{x^2 - x}{2}\right)$')
+    bottom_left.text(-2.5, -2, label3, fontsize=fontsize)
+    x_vals = np.linspace(0, 2, num_pts)
+    y_vals = 3 * x_vals**2 - 6 * x_vals + 2
+    bottom_left.plot(x_vals, y_vals, color=colors[2])
+    if stopping_point == 7:
+        return
+    # Do a little bit extra: a dot at 0.5.
+    y_val3 = 3 * 0.5**2 - 6 * 0.5 + 2
+    bottom_left.plot([0.5], [y_val3], color=colors[2],
+                     linestyle='None', marker='o')
+    label3_half = (r'$\frac{3}{8}u_{-1} + \frac{3}{4}u_{0} - '
+                   r'\frac{1}{8}u_{1}$')
+    bottom_left.text(-2.5, 2.75, label3_half, fontsize=fontsize)
+    if stopping_point == 8:
+        return
+
+    # Bottom right plot (-2, -1, 0, 1 and 2)
+    bottom_right = ax_vals[1, 1]
+    bottom_right.plot(np.linspace(-2.5, -1.5, num_pts),
+                      0 * np.ones(num_pts),
+                      color='black', linestyle='dashed')
+    bottom_right.plot(np.linspace(-1.5, -0.5, num_pts),
+                      3 * np.ones(num_pts),
+                      color='black', linestyle='dashed')
+    bottom_right.plot(np.linspace(-0.5, 0.5, num_pts),
+                      2 * np.ones(num_pts),
+                      color='black', linestyle='dashed')
+    bottom_right.plot(np.linspace(0.5, 1.5, num_pts),
+                      -1 * np.ones(num_pts),
+                      color='black', linestyle='dashed')
+    bottom_right.plot(np.linspace(1.5, 2.5, num_pts),
+                      2 * np.ones(num_pts),
+                      color='black', linestyle='dashed')
+    if stopping_point == 9:
+        return
+    x_vals = np.linspace(-2, 2, num_pts)
+    y_vals = (3 * x_vals**4 + 10 * x_vals**3 - 15 * x_vals**2 -
+              34 * x_vals + 24) / 12.0
+    bottom_right.plot(x_vals, y_vals, color=colors[3])
+    if stopping_point == 10:
+        return
+    # Do a little bit extra: a dot at 0.5.
+    y_val4 = (3 * 0.5**4 + 10 * 0.5**3 - 15 * 0.5**2 -
+              34 * 0.5 + 24) / 12.0
+    bottom_right.plot([0.5], [y_val4], color=colors[3],
+                      linestyle='None', marker='o')
+    label4_half = (r'$\frac{3}{128}u_{-2} - \frac{5}{32}u_{-1} + '
+                   r'\frac{45}{64}u_{0} + \frac{15}{32}u_{1} -'
+                   r'\frac{5}{128}u_{2}$')
+    bottom_right.text(-2.5, -2, label4_half, fontsize=fontsize)
+    if stopping_point == 11:
+        return
+    # Do a little bit more: add all the other dots.
+    bottom_right.plot([0.5], [y_val1], color=colors[0],
+                      linestyle='None', marker='o')
+    bottom_right.plot([0.5], [y_val2], color=colors[1],
+                      linestyle='None', marker='o')
+    bottom_right.plot([0.5], [y_val3], color=colors[2],
+                      linestyle='None', marker='o')
